7d70044dba1279e7b390a0c051afcb873db6f84f
Emílio Silva <emiliosarges@gmail.com>
2025-06-25 01:25:12 -0300

    up

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,7 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Primeiro Programa Parte #01");
+		System.out.println("Primeiro Programa Parte #01");
 	}
 }
