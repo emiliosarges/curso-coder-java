9e69fdb4d247a8bf6f9e633a2d6069938b909ad4
Emílio Silva <emiliosarges@gmail.com>
2025-06-26 00:56:20 -0300

    basico
    Revisando o básico

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,23 @@
 package fundamentos;
 
+/**
+ * JavaDoc
+ * 
+ * @author emiliosilva
+ * @since 1.0
+ */
+
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Primeiro Programa Parte #01");
 		System.out.println("Primeiro Programa Parte #01");
 	}
 }
+
+/*
+ * comentário
+ * comentário
+ */
+
+//comentário 1 linha
+//mais uma linha
