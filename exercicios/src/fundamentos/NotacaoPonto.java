d88d0dd143bef14da752c75380178f002d731064
Emílio Silva <emiliosarges@gmail.com>
2025-07-01 01:00:18 -0300

    up

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		
+		String s = "Bom dia X";
+		s = s.replace("X", "Senhora");
+		s = s.toUpperCase();
+		s = s.concat("!!!");
+		
+		System.out.println(s);
+		
+		System.out.println("Emilio".toUpperCase());
+		
+		String sobreNome = "Silva".toLowerCase();
+		System.out.println(sobreNome);
+		
+		String y = "Bom dia X"
+				.replace("X", "Gui")
+				.toUpperCase()
+				.concat("!!!");
+		System.out.println(y);
+	}
+}
